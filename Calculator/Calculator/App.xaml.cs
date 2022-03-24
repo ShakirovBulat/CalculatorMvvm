b3f92f28bd9b0ecd900df5b5423128e8d0cfaa1d
булат шакиров <a2009-1@mail.ru>
2022-03-24 19:20:09 +0300

    Добавьте файлы проекта.

---
+++ Calculator/Calculator/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Calculator
+{
+    public partial class App : Application
+    {
+        CalculatorMvvm add;
+        public App()
+        {
+            InitializeComponent();
+            add = new CalculatorMvvm();
+            add.RestoreState(Current.Properties);
+
+            MainPage = new MainPage(add);
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
